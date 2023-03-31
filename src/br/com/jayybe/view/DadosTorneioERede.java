773540752be43ff3aa583e714b7812cac577ec3c
Josué Borges <jborges.ads@gmail.com>
2023-03-31 16:34:41 -0300

    Estrutura Inicial

---
+++ src/br/com/jayybe/view/DadosTorneioERede.java
@@ -0,0 +1,23 @@
+package br.com.jayybe.view;
+
+public class DadosTorneioERede {
+
+	private Long torneio;
+	private String rede;
+
+	public Long getTorneio() {
+		return torneio;
+	}
+
+	public void setTorneio(Long torneio) {
+		this.torneio = torneio;
+	}
+
+	public String getRede() {
+		return rede;
+	}
+
+	public void setRede(String rede) {
+		this.rede = rede;
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author HOME-PC
+ *
+ */
+module Sharkscope {
+	requires java.desktop;
+	requires org.apache.poi.poi;
+	requires org.apache.poi.ooxml;
+	requires org.seleniumhq.selenium.api;
+	requires org.seleniumhq.selenium.chrome_driver;
+	requires selenium.api;
+	requires selenium.chrome.driver;
+	requires selenium.support;
+}
