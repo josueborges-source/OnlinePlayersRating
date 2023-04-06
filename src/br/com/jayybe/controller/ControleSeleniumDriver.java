e26ce1dce5408d8362f24908b959f77423df0c14
Josué Borges <jborges.ads@gmail.com>
2023-04-06 20:56:51 -0300

    Refatoracoes: Divisoes entre Apache Poi e Selenium

---
+++ src/br/com/jayybe/controller/ControleSeleniumDriver.java
@@ -0,0 +1,62 @@
+package br.com.jayybe.controller;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ControleSeleniumDriver {
+	
+	
+	private WebDriver driver = new ChromeDriver();
+	
+	ControleSeleniumDriver(WebDriver driver){
+		this.driver = driver;
+	}
+	
+	public List<WebElement> retornaElementosSelectDaPagina()
+	{
+		ArrayList<WebElement> elementosSelect = new ArrayList<WebElement>();
+		
+		elementosSelect = (ArrayList<WebElement>) ((JavascriptExecutor) driver)
+		.executeScript("return document.querySelectorAll(\"select[title='Records per Page']\");");
+		
+		return elementosSelect;
+	}
+	
+	public void alteraElementoSelectPara20000(WebElement selectElement)
+	{		
+		// Altera o valor do último option para "20000"
+		((JavascriptExecutor) driver).executeScript("var options = arguments[0].getElementsByTagName('option');"
+				+ "options[options.length - 1].value = '20000';" + "options[options.length - 1].text = '20000';",
+				selectElement);
+	}
+
+	public void clicaNaUltimaOpcaoDeElementoSelect(WebElement selectElement) {
+		((JavascriptExecutor) driver).executeScript("var options = arguments[0].getElementsByTagName('option');"
+				+ "options[options.length - 1].selected = true;" + "arguments[0].dispatchEvent(new Event('change'));",
+				selectElement);		
+	}
+	
+	public List<WebElement> retornaElementosIdJqg() {
+		List<WebElement> linhasTabela = driver.findElements(By.cssSelector("[id^='jqg']"));
+		
+		System.out.println("Elementos jg(Jogador):: " + linhasTabela.size());
+		
+		return linhasTabela;
+	}
+
+	public void imprimaValoresDasLinhasDaTabela(List<WebElement> linhasTabela) {
+		for (WebElement webElement : linhasTabela) {
+		    WebElement tdNome = webElement.findElement(By.xpath(".//td[4]//a[@class='playerlink']"));
+		    System.out.println("Elemento encontrado: " + tdNome);
+		    String nomeJogador = tdNome.getText();
+		    System.out.println("Nome do jogador: " + nomeJogador);
+		}		
+	}	
+	
+}
---
+++ src/br/com/jayybe/controller/Log.java
@@ -0,0 +1,69 @@
+package br.com.jayybe.controller;
+
+import java.awt.Color;
+
+import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+
+import br.com.jayybe.view.TelaPrincipal;
+
+public class Log {
+	
+	private static JTextPane textPaneLogDoSharkscope;
+	private static int contadorLinhas = 0;
+	
+	private static Color[] cores = new Color[] { Color.BLUE, Color.RED, Color.GREEN, Color.ORANGE, Color.MAGENTA, Color.CYAN };
+
+	
+	
+	public static void acaoParaLog(String mensagemParaInserirEmLog) 
+	{			
+		SwingUtilities.invokeLater(new Runnable() {
+	        public void run() {
+	            // Obtém o modelo do documento do JTextPane
+	            StyledDocument doc = textPaneLogDoSharkscope.getStyledDocument();
+
+	            // Define o estilo do texto para a linha atual
+	            SimpleAttributeSet estilo = new SimpleAttributeSet();
+	            StyleConstants.setForeground(estilo, cores[contadorLinhas % cores.length]);
+
+	            // Adiciona o texto com o estilo configurado
+	            try {
+	                doc.insertString(doc.getLength(), "•", estilo);
+	                doc.insertString(doc.getLength(), mensagemParaInserirEmLog + "\n", estilo);
+	            } catch (BadLocationException e) {
+	                e.printStackTrace();
+	            }
+
+	            contadorLinhas++;
+
+	            // Executa o atraso em uma nova thread
+	            new Thread(new Runnable() {
+	                public void run() {
+	                    try {
+	                        Thread.sleep(50);
+	                    } catch (InterruptedException e) {
+	                        e.printStackTrace();
+	                    }
+	                    // Atualiza o JTextPane
+	                    SwingUtilities.invokeLater(new Runnable() {
+	                        public void run() {
+	                            textPaneLogDoSharkscope.revalidate();
+	                            textPaneLogDoSharkscope.repaint();
+	                        }
+	                    });
+	                }
+	            }).start();
+	        }
+	    });
+		
+		TelaPrincipal.frame.revalidate();
+		TelaPrincipal.frame.repaint();
+	}
+	
+	
+}
